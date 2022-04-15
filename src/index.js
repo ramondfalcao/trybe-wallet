69c4d53adc95b6ce3e7de62ca471e4bff20b7822
Ramond Falcão <ramondfalcao@gmail.com>
2022-04-15 16:22:49 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -16,11 +16,4 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// <Provider store={ store }>
-// <App />
-// </Provider>
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
