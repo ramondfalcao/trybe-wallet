a33909780cec2b00e5ea2b755ee8ea962b3eb4b9
Ramond Falcão <ramondfalcao@gmail.com>
2022-04-15 16:14:19 -0300

    Updates

---
+++ precache-manifest.d4fd2aa067b9f7202ab3fd7cba681ab5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10688b009f464299828dab4593871de4",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "08e4f6501489a21fa095",
+    "url": "/trybe-wallet/static/css/main.1f2b9392.chunk.css"
+  },
+  {
+    "revision": "16e43e2d50286d371cac",
+    "url": "/trybe-wallet/static/js/2.014f6dcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.014f6dcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e4f6501489a21fa095",
+    "url": "/trybe-wallet/static/js/main.7d389a75.chunk.js"
+  },
+  {
+    "revision": "59d3c1ec903278203d08",
+    "url": "/trybe-wallet/static/js/runtime-main.2e2bea36.js"
+  },
+  {
+    "revision": "84eb2fec3f53e8aa0c73090bf1c7ab46",
+    "url": "/trybe-wallet/static/media/coin.84eb2fec.svg"
+  },
+  {
+    "revision": "8538d153d6c1517b55fdb8552d06807a",
+    "url": "/trybe-wallet/static/media/money.8538d153.svg"
+  }
+]);
